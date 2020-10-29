59cdb11a21b163f8865be605bfdd2cd0e5147c32
n-deleforge <contact@nicolas-deleforge.fr>
2020-10-29 11:04:08 +0100

    Update of the get function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1227,21 +1227,21 @@ function resetData() {
 // =================================================
 // ============ GENERIC
 
-// ===> Give a number between min and max
-function nbRandom(min, max) {
-    return (Math.floor(Math.random() * Math.floor(max))) + min;
-}
-
-// ===> Select an element
+// ===> Easy selector
 function get(n) {
-    if (n.search("#") != -1 && document.getElementById(n.split("#")[1]) != null) { return document.getElementById(n.split("#")[1]); }
-    if (n.search("~") != -1 && document.querySelectorAll(n.split('~')[1]) != null) { return document.querySelectorAll(n.split('~')[1]); }
-    if (n.search("\\.") != -1 && document.querySelectorAll(n).length != 0) { return document.querySelectorAll(n); }
+    if (n.search("#") == 0 && n.split("#")[1] != null && document.querySelector(n) != null) return document.querySelector(n);
+    if (n.search(".") == 0 && n.split(".")[1] != null && document.querySelectorAll(n) != null) return document.querySelectorAll(n);
+    if (n.search("~") == 0 && n.split("~")[1] != null && document.querySelectorAll(n.split("~")[1]) != null) return document.querySelectorAll(n.split("~")[1])[0];
 }
 
-// ===> Fast usage of localStorage
+// ===> Simplier usage of the local storage
 function storage(a, n, v) {
     if (a == "get") return localStorage.getItem(n);
     if (a == "set") return localStorage.setItem(n, v);
     if (a == "rem") return localStorage.removeItem(n);
+}
+
+// ===> Give a random number
+function nbRandom(min, max) {
+    return (Math.floor(Math.random() * Math.floor(max))) + min;
 }
